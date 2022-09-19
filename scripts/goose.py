3f474c778d4e14e8e3dffbb6e28cace6ad875e9f
ckgresla <ckgresla@gmail.com>
2022-09-19 16:16:21 -0500

    quality of life improvements, i.e better print statements in Goose and Newspaper parsers

--- scripts/goose.py
+++ scripts/goose.py
@@ -6,14 +6,15 @@ url = "https://en.wikipedia.org/wiki/Reinforcement_learning"
 url = "https://medium.com/analytics-vidhya/abstractive-text-summarization-430901a602c0"
 url = "https://arxiv.org/pdf/2108.03350.pdf"
 url = "https://lilianweng.github.io/lil-log/contact.html"
-url = "https://lilianweng.github.io/lil-log/contact.html"
 url = "https://web.stanford.edu/group/pdplab/pdphandbook/handbookch10.html"
 url = "https://medium.com/war-is-boring/the-u-s-air-force-once-sent-drones-on-daring-suicide-missions-over-vietnam-cd2895cc0c8b"
+url = "https://www.tutorialspoint.com/How-to-print-all-the-keys-of-a-dictionary-in-Python"
+url = 'http://fox13now.com/2013/12/30/new-year-new-laws-obamacare-pot-guns-and-drones/'
 g = Goose()
 article = g.extract(url=url)
 
-print(f"Title: {article.title}")
+print(f"\nTitle:\n{article.title}")
 #print(f"Description: {article.meta_description}")
-print(f"Clean Text: {article.cleaned_text}")
+print(f"\nClean Text:\n{article.cleaned_text}\n")
 
 
--- scripts/newspaper_parser.py
+++ scripts/newspaper_parser.py
@@ -2,13 +2,19 @@
 # need do a `pip3 install newspaper3k` to get it in the env (also need run w python3)
 from newspaper import Article
 
-url = 'http://fox13now.com/2013/12/30/new-year-new-laws-obamacare-pot-guns-and-drones/'
 url = 'https://en.wikipedia.org/wiki/Reinforcement_learning'
+url = 'http://fox13now.com/2013/12/30/new-year-new-laws-obamacare-pot-guns-and-drones/'
+url = 'https://brain.ai'
+
+
 article = Article(url)
 
 article.download()
 article.parse()
-info = [article.authors, article.text]
+info = [article.authors, article.title, article.text]
 
 for i in info:
-	print(i)
+	print(i, "\n\n")
+
+
+#print(dir(article))
