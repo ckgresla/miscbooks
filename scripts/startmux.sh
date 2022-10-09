c7323096d7d7162f565a00a026ddcc290ca0c256
ckgresla <ckgresla@gmail.com>
2022-10-09 11:00:31 -0500

    catppuccin color for ssh bar in startmux too bright, switching back

--- scripts/startmux.sh
+++ scripts/startmux.sh
@@ -7,8 +7,8 @@ tmux send-keys -t General 'tmux set status-style "bg=default"' Enter''
 tmux send-keys -t General 'clear' Enter''
 
 tmux new-session -d -s  ssh #add ssh session
-#tmux send-keys -t ssh 'tmux set status-style "bg=#2942ff"' Enter'' #make bg of statusbar a bright blue
-tmux send-keys -t ssh 'tmux set status-style "bg=#74c7ec"' Enter'' #make bg of statusbar catppuccin's sapphire
+tmux send-keys -t ssh 'tmux set status-style "bg=#2942ff"' Enter'' #make bg of statusbar a bright blue
+#tmux send-keys -t ssh 'tmux set status-style "bg=#74c7ec"' Enter'' #make bg of statusbar catppuccin's sapphire
 tmux send-keys -t ssh 'clear' Enter''
 
 
