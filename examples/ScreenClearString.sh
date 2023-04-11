b0195600dc8fe9d9c6c08cd9417cf9346491b1ed
Chris Kerwell Gresla <chris@lindy.ai>
2023-04-11 14:10:18 -0500

    screen clear string/cmd for future ref

---
+++ examples/ScreenClearString.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Clear out a screen with this string (could print from python or some scripting lang) -- effectively same as `clear` command or ctrl+l keybinding
+
+echo '\x1bc'
+#run the above at the command line
