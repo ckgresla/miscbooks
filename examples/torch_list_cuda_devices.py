ca241ce4890ee40c9250ae4c9c8a4a60c8795d75
Chris Kerwell Gresla <chris@lindy.ai>
2023-05-30 12:07:27 -0500

    better torch device list script -- has VRAM per device included in print

--- examples/torch_list_cuda_devices.py
+++ examples/torch_list_cuda_devices.py
@@ -1,12 +1,17 @@
+#!/usr/bin/env python
 # Find all CUDA Compute Devices & Print Names per Index
 import torch
 
 if torch.cuda.is_available():
     print("CUDA detected, you are rich")
     for device_id in range(torch.cuda.device_count()):
-        print(f"Device Index: {device_id} -- {torch.cuda.get_device_name(device_id)}")
+        memory_gb = torch.cuda.get_device_properties(device_id).total_memory / 1000000000
+        print(f"Device Index: {device_id} -- {torch.cuda.get_device_name(device_id)} has {memory_gb:.2f}GB of VRAM")
 
 else:
-    print("CUDA not detected, not rich")
+    print("CUDA not detected, fail to be rich")
+
+
+
 
 
