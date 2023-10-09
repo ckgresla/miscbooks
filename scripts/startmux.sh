4ff7af362b4094caf439f279ed6015863c585277
Chris Kerwell Gresla <ckgresla@gmail.com>
2023-10-09 10:53:42 -0700

    better catppuccin colors for the alt tmux window statusbar

--- scripts/startmux.sh
+++ scripts/startmux.sh
@@ -10,8 +10,11 @@ tmux send-keys -t General 'clear' Enter''
 
 tmux new-session -d -s  ssh #add ssh session
 # tmux send-keys -t ssh 'tmux set status-style "bg=#2942ff"' Enter'' #make bg of statusbar a bright blue
-# tmux send-keys -t ssh 'tmux set status-style "bg=#74c7ec"' Enter'' #make bg of statusbar catppuccin's sapphire
-tmux send-keys -t ssh 'tmux set status-style "bg=#b4befe"' Enter'' #catppucin lavender
+tmux send-keys -t ssh 'tmux set status-style "bg=#7287fd"' Enter'' #catppucin latte - lavender
+tmux send-keys -t ssh 'tmux set status-style "bg=#dd7878"' Enter'' #catppucin latte - flamingo
+tmux send-keys -t ssh 'tmux set status-style "bg=#dc8a78"' Enter'' #catppucin latte - rosewater
+tmux send-keys -t ssh 'tmux set status-style "bg=#1e66f5"' Enter'' #catppucin latte - blue
+
 tmux send-keys -t ssh 'clear' Enter''
 
 
