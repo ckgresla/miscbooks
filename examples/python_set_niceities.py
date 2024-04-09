4c3452b8cb7297b3953bff1182a51302a9c043c2
Chris Kerwell Gresla <ckgresla@gmail.com>
2024-04-09 13:47:34 -0700

    set ops

---
+++ examples/python_set_niceities.py
@@ -0,0 +1,21 @@
+# you have some pretty NICE operators available for doing things with sets...
+# see- https://arc.net/l/quote/qzblextw
+
+
+set1 = {1, 2, 3}
+set2 = {3, 4, 5}
+
+# Union
+print(set1 | set2)  # Output: {1, 2, 3, 4, 5}
+
+# Intersection
+print(set1 & set2)  # Output: {3}
+
+# Difference
+print(set1 - set2)  # Output: {1, 2}
+
+# Symmetric Difference
+print(set1 ^ set2)  # Output: {1, 2, 4, 5}
+
+
+
