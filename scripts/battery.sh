05b2b3221fb9b9c1fbe4a92cccc033a2d866135b
Chris Kerwell Gresla <ckgresla@gmail.com>
2024-10-21 23:04:26 -0700

    feat: lowercase general and % sign for tmux statusbar

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-pmset -g batt | grep "[0-9][0-9]%" | awk "NR==1{print$3}" | cut -c 34-36
-	#a Script to get the current battery percentage (mac specific utility)
+# a Script to get the current battery percentage (mac specific utility)
+
+export battery_percentage=$(pmset -g batt | grep "[0-9][0-9]%" | awk "NR==1{print$3}" | cut -c 34-36)
+echo "${battery_percentage}%"
+
--- scripts/startmux.sh
+++ scripts/startmux.sh
@@ -2,22 +2,22 @@
 # Startup Script to get Tmux Working and looking pretty
 
 
-tmux new-session -d -s General # start session with 1st window
-tmux send-keys -t General 'tmux rename-window main' Enter''
-tmux send-keys -t General 'tmux set status-style "bg=default"' Enter''
-tmux send-keys -t General 'tmux rename-window main' Enter''
-tmux send-keys -t General 'clear' Enter''
+tmux new-session -d -s general # start session with 1st window
+tmux send-keys -t general 'tmux rename-window main' Enter''
+tmux send-keys -t general 'tmux set status-style "bg=default"' Enter''
+tmux send-keys -t general 'tmux rename-window main' Enter''
+tmux send-keys -t general 'clear' Enter''
 
 tmux new-session -d -s  ssh #add ssh session
 # tmux send-keys -t ssh 'tmux set status-style "bg=#2942ff"' Enter'' #make bg of statusbar a bright blue
-# tmux send-keys -t ssh 'tmux set status-style "bg=#7287fd"' Enter'' #catppucin latte - lavender
+tmux send-keys -t ssh 'tmux set status-style "bg=#7287fd"' Enter'' #catppucin latte - lavender
 # tmux send-keys -t ssh 'tmux set status-style "bg=#dd7878"' Enter'' #catppucin latte - flamingo
 # tmux send-keys -t ssh 'tmux set status-style "bg=#dc8a78"' Enter'' #catppucin latte - rosewater
-tmux send-keys -t ssh 'tmux set status-style "bg=#1e66f5"' Enter'' #catppucin latte - blue
+# tmux send-keys -t ssh 'tmux set status-style "bg=#1e66f5"' Enter'' #catppucin latte - blue
 
 tmux send-keys -t ssh 'clear' Enter''
 
 
-tmux attach-session -d -t General #time to rock
+tmux attach-session -d -t general #time to rock
 
 
